6b793ef8568785f9304ae572b093b2cade0984eb
SuperCode Gnertic <ignertic@icloud.com>
2020-08-02 16:23:38 +0200

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -50,68 +51,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              forceElevated: true,
+              expandedHeight: 100.0,
+              floating: true,
+              elevation: 50,
+              title: Text('Explore'),
+              leading: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Text('CH',style: TextStyle(
+                      color: Colors.white
+                  ),)
+              ),
+              backgroundColor: Colors.green,
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                  onPressed: (){
+
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.alarm,
+                    color: Colors.black,
+                  ),
+                  onPressed: (){
+
+                  },
+                )
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                title: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                    children: [
+                      TextSpan(text : 'Course', style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black
+                      )),
+                      TextSpan(text: 'hub',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w600
+                          )
+                      )
+                    ]
+                )),
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
